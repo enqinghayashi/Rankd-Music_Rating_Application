7747e4337add6e1f51b409afadc262df06bb93da
= <archerbkey@gmail.com>
2025-04-21 13:25:43 +0800

    converted html pages to extend base.html to work with flask

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,8 @@ from app import app
 @app.route('/index')
 @app.route('/home')
 def index():
-  return render_template("home.html", title="Home")
+  return render_template("index.html", title="Home")
+
+@app.route('/score')
+def score():
+  return render_template("score.html", title="Score")
