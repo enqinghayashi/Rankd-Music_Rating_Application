984a20f47d858fe2bf6c7d960ea25420d4d64f2a
= <archerbkey@gmail.com>
2025-04-22 14:34:26 +0800

    added __repr__ methods to db models to make debugging these models easier

--- app/models.py
+++ app/models.py
@@ -10,6 +10,9 @@ class User(db.Model):
   email = db.Column(db.String, nullable=False)
   password = db.Column(db.String, nullable=False)
 
+  def __repr__(self):
+    return 'user_id={}, username={}, email={}, password={}'.format(self.user_id, self.username, self.email, self.password)
+
 class Score(db.Model):
   __table_name__ = 'Score'
 
@@ -18,6 +21,7 @@ class Score(db.Model):
   user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), primary_key=True)
   # From the spotify API
   item_id = db.Column(db.String, primary_key=True)
+  item_type = db.Column(db.String, nullable=False)
   title = db.Column(db.String, nullable=False)   
   creator = db.Column(db.String, nullable=False) 
   img_url = db.Column(db.String, nullable=False)
@@ -26,4 +30,8 @@ class Score(db.Model):
   #__table_args__ = (
   #  PrimaryKeyConstraint('user_id', 'item_id')
   #)
+
+  def __repr__(self):
+    return 'score={}, title={}, creator={}, item_type={}, img_url={}, user_id={}, item_id={}\
+      '.format(self.score, self.title, self.creator, self.item_type, self.img_url, self.user_id, self.item_id)
   
