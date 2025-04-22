c4970e9877e21cb381cc8cafb8b10f3a9114e6bc
enqinghayashi <enqinglin95@gmail.com>
2025-04-22 15:50:09 +0800

    stored SECRET KEY here, and added PATH for where profile pictures will be saved and  the allowed file types for picture upload

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
-#import os
+import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 #
-#class Config(object):
+class Config:
+    SERCRET_KEY = 'admin3403'
+    UPLOAD_FOLDER = 'app/static/img/profile_pictures'
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 #  # ...
 #  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
