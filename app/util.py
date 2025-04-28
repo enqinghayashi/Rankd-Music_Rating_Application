379f1ab54b0b6a33f2697d5d752837d0c206514d
enqinghayashi <enqinglin95@gmail.com>
2025-04-29 00:19:44 +0800

    created a new validator named validate_email, for validating the email using regex logic

--- app/util.py
+++ app/util.py
@@ -6,18 +6,22 @@ def validate_password(password, confirm_password=None, route_name=None):
     regex = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!#$%^&*])[A-Za-z\d@!#$%^&*]{8,}$'
 
     if not re.match(regex, password):
-        flash("Password must contain at least 1 letter, 1 number, and 1 special character.", "error")
-        if route_name:
-            return redirect(url_for(route_name))
+        flash("Password must contain at least 1 letter, 1 number, and 1 special character.", "danger")
+        return "Password does not meet the requirement"
+
 
     if len(password) < 8:
-        flash("Password must be at least 8 characters long.", "error")
-        if route_name:
-            return redirect(url_for(route_name))
+        flash("Password must be at least 8 characters long.", "danger")
+        return "Password is too short"
 
     if confirm_password and password != confirm_password:
-        flash("Confirmed passwords do not match.", "error")
-        if route_name:
-            return redirect(url_for(route_name))
-
+        flash("Confirmed passwords do not match.", "danger")
+        return "Password does not match"
     return None 
+
+def validate_email(email):
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if not re.match(email_regex, email):
+        flash("Invalid email address", "danger")
+        return "Email address seems fake"
+    return None
