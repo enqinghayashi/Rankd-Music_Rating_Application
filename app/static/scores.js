baafa57fd6e4eb5a5e16cc4889dd11529c22cc12
= <archerbkey@gmail.com>
2025-05-02 17:17:24 +0800

    added skeleton functions for scores.js to clarify data flow

---
+++ app/static/scores.js
@@ -0,0 +1,25 @@
+// Get the contents of the search bar and search options
+function getSearchParameters() {
+
+}
+
+// Make a request to the server for the items
+function getItems() {
+
+}
+
+// Remove the currently loaded items
+function clearScores() {
+
+}
+
+// Create an item from data
+function createItem() {
+
+}
+
+// Render the items on the page
+function renderItems() {
+
+}
+
