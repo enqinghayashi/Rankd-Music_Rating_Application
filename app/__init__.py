956e44bcb09dacb6b655d497e0b0b015f44823fe
enqinghayashi <enqinglin95@gmail.com>
2025-05-10 00:37:50 +0800

    fixed code execution order that resulted bug

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,17 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager
-from app.models import User
 app = Flask(__name__)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app import routes, models
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+from app.models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-from app import routes, models
