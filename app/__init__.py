897358d23de7ab77f7a34c89b41343acf14b0cd0
enqinghayashi <enqinglin95@gmail.com>
2025-05-09 22:51:17 +0800

    imported user to avoid reference errors

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,18 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager
-
+from app.models import User
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
 
 
 from app import routes, models
